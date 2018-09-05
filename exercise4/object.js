e10b679788c76087f79c41a971e66c771bfbfed6
Sebastian Clinton <sebastian@Dougs-iMac.home>
2018-09-05 14:57:22 +0100

    added prototype functions

--- exercise4/object.js
+++ exercise4/object.js
@@ -12,3 +12,33 @@ function Asteroid(segments, radius, noise) {
         this.shape.push(Math.random() - 0.5);
     }
 }
+
+Asteroid.prototype.update = function(elapsed) {
+    if(this.x - this.radius + elapsed * this.x_speed > context.canvas.width) {
+        this.x = -this.radius;
+    }
+    if(this.x + this.radius + elapsed * this.x_speed < 0) {
+        this.x = context.canvas.width + this.radius;
+    }
+    if(this.y - this.radius + elapsed * this.y_speed > context.canvas.height) {
+        this.y = -this.radius;
+    }
+    if(this.y - this.radius + elapsed * this.y_speed < 0) {
+        this.y = context.canvas.height + this.radius;
+    }
+    this.x += elapsed * this.x_speed;
+    this.y += elapsed * this.y_speed;
+    this.angle = (this.angle + this.rotation_speed * elapsed) %
+    (2 * Math.PI);
+}
+
+Asteroid.prototype.draw = function(ctx, guide) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    draw_asteroid(ctx, this.radius, this.shape, {
+        guide: guide,
+        noise: this.noise
+    });
+    ctx.restore();
+}

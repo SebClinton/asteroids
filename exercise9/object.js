852ea8f7e812f4061eae0b1150c13516530096b6
Sebastian Clinton <sebastian@Hadron.home>
2018-10-01 19:05:51 +0100

    created an extend method and worked with the asteroid class

--- exercise9/object.js
+++ exercise9/object.js
@@ -1,17 +1,24 @@
-function Asteroid(segments, radius, noise) {
-    this.x = context.canvas.width * Math.random();
-    this.y = context.canvas.height * Math.random();
-    this.angle = 0;
-    this.x_speed = context.canvas.width * (Math.random() - 0.5);
-    this.y_speed = context.canvas.height * (Math.random() - 0.5);
-    this.rotation_speed = 2 * Math.PI * (Math.random() - 0.5);
-    this.radius = radius;
-    this.noise = noise;
+function extend(ChildClass, ParentClass) {
+    var parent = new ParentClass();
+    ChildClass.prototype = parent;
+    ChildClass.prototype.super = parent.constructor;
+    ChildClass.prototype.constructor = ChildClass;
+}
+
+function Asteroid(mass, x, y, x_speed, y_speed, rotation_speed) {
+    var density = 1; // kg per square pixel
+    var radius = Math.sqrt((mass / density) / Math.PI);
+    this.super(mass, radius, x, y, 0, x_speed, y_speed, rotation_speed);
+    this.circumference = 2 * Math.PI * this.radius;
+    this.segments = Math.ceil(this.circumference / 15);
+    this.segments = Math.min(25, Math.max(5, this.segments));
+    this.noise = 0.2;
     this.shape = [];
-    for (var i = 0; i < segments; i++) {
-        this.shape.push(Math.random() - 0.5);
+    for(var i = 0; i < this.segments; i++) {
+        this.shape.push(2 * (Math.random() - 0.5));
     }
 }
+extend(Asteroid, Mass);
 
 Asteroid.prototype.update = function (elapsed) {
     if (this.x - this.radius + elapsed * this.x_speed > context.canvas.width) {

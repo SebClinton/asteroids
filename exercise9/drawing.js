26a0a7f50e4eb890bdeee5f1d8fb5e9d95d2ffa1
Sebastian Clinton <sebastian@Hadron.home>
2018-10-03 21:18:04 +0100

    formatted

--- exercise9/drawing.js
+++ exercise9/drawing.js
@@ -1,44 +1,48 @@
-
 function draw_grid(ctx, minor, major, stroke, fill) {
-    minor = minor  || 10;
+    minor = minor || 10;
     major = major || minor * 5;
     stroke = stroke || "#00FF00";
     fill = fill || "#009900";
     ctx.save();
     ctx.strokeStyle = stroke;
     ctx.fillStyle = fill;
-    let width = ctx.canvas.width, height = ctx.canvas.height
-    for(var x = 0; x < width; x += minor) {
+    let width = ctx.canvas.width,
+        height = ctx.canvas.height
+    for (var x = 0; x < width; x += minor) {
         ctx.beginPath();
         ctx.moveTo(x, 0);
         ctx.lineTo(x, height);
         ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
         ctx.stroke();
-        if(x % major == 0) {ctx.fillText(x, x, 10);}
+        if (x % major == 0) {
+            ctx.fillText(x, x, 10);
+        }
     }
-    for(var y = 0; y < height; y += minor) {
+    for (var y = 0; y < height; y += minor) {
         ctx.beginPath();
         ctx.moveTo(0, y);
         ctx.lineTo(width, y);
         ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
         ctx.stroke();
-        if(y % major == 0) {ctx.fillText(y, 0, y + 10);}
+        if (y % major == 0) {
+            ctx.fillText(y, 0, y + 10);
+        }
     }
     ctx.restore();
 }
 
-function draw_ship(ctx, radius, options){
+function draw_ship(ctx, radius, options) {
     options = options || {};
-    let angle = (options.angle || 0.5 * Math.PI) /2;
+    let angle = (options.angle || 0.5 * Math.PI) / 2;
     let curve1 = options.curve1 || 0.25;
     let curve2 = options.curve2 || 0.75;
     ctx.save();
-    if(options.guide){
+    if (options.guide) {
         ctx.strokeStyle = "white";
         ctx.fillStyle = "rgba(0,0,0,0.25";
         ctx.lineWidth = 0.5;
         ctx.beginPath();
-        ctx.arc(0,0,radius,0,2 * Math.PI);
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.fill();
     }
@@ -46,7 +50,7 @@ function draw_ship(ctx, radius, options){
     ctx.strokeStyle = options.stroke || "white";
     ctx.fillStyle = options.fill || "black";
     ctx.beginPath();
-    ctx.moveTo(radius,0);
+    ctx.moveTo(radius, 0);
     ctx.quadraticCurveTo(
         Math.cos(angle) * radius * curve2,
         Math.sin(angle) * radius * curve2,
@@ -64,7 +68,7 @@ function draw_ship(ctx, radius, options){
     );
     ctx.fill();
     ctx.stroke();
-    if(options.guide) {
+    if (options.guide) {
         ctx.strokeStyle = "white";
         ctx.fillStyle = "white";
         ctx.beginPath();
@@ -72,33 +76,33 @@ function draw_ship(ctx, radius, options){
             Math.cos(-angle) * radius,
             Math.sin(-angle) * radius
         );
-        ctx.lineTo(0,0);
+        ctx.lineTo(0, 0);
         ctx.lineTo(
             Math.cos(angle) * radius,
             Math.sin(angle) * radius
         );
         ctx.moveTo(-radius, 0);
-        ctx.lineTo(0,0);
+        ctx.lineTo(0, 0);
         ctx.stroke();
         ctx.beginPath();
         ctx.arc(
             Math.cos(angle) * radius * curve2,
             Math.sin(angle) * radius * curve2,
-            radius/40, 0, 2 * Math.PI
+            radius / 40, 0, 2 * Math.PI
         );
         ctx.fill();
         ctx.beginPath();
         ctx.arc(
             Math.cos(angle) * radius * curve2,
             Math.sin(angle) * radius * curve2,
-            radius/40, 0, 2 * Math.PI
+            radius / 40, 0, 2 * Math.PI
         );
         ctx.fill();
         ctx.beginPath();
-        ctx.arc(radius * curve1 - radius, 0, radius/50, 0, 2 * Math.PI);
+        ctx.arc(radius * curve1 - radius, 0, radius / 50, 0, 2 * Math.PI);
         ctx.fill();
     }
-   
+
     ctx.restore();
 }
 
@@ -124,29 +128,29 @@ function draw_asteroid(ctx, radius, shape, options) {
     ctx.fillSyle = options.fill || "black";
     ctx.save();
     ctx.beginPath();
-    for(let i = 0; i < shape.length; i++) {
+    for (let i = 0; i < shape.length; i++) {
         ctx.rotate(2 * Math.PI / shape.length);
         ctx.lineTo(radius + radius * options.noise * shape[i], 0);
     }
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
-    if(options.guide) {
+    if (options.guide) {
         ctx.lineWidth = 0.5;
         ctx.beginPath();
-        ctx.arc(0,0,radius,0,2 * Math.PI);
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.beginPath();
         ctx.lineWidth = 0.2;
-        ctx.arc(0,0,radius + radius * options.noise, 0, 2 * Math.PI);
+        ctx.arc(0, 0, radius + radius * options.noise, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.beginPath();
-        ctx.arc(0,0, radius - radius * options.noise, 0,2 * Math.PI);
+        ctx.arc(0, 0, radius - radius * options.noise, 0, 2 * Math.PI);
         ctx.stroke();
     }
     ctx.restore();
 }
-     
+
 function draw_ghost(ctx, radius, options) {
     options = options || {}
     var feet = options.feet || 4;
@@ -156,7 +160,7 @@ function draw_ghost(ctx, radius, options) {
     var pupil_radius = eye_radius / 4;
 
     var left_eye_centre_x = -head_radius / 2.7;
-    var left_eye_centre_y = -head_radius * 0.4  ;
+    var left_eye_centre_y = -head_radius * 0.4;
     var right_eye_centre_x = head_radius / 2.7;
     var right_eye_centre_y = -head_radius * 0.4
 
@@ -165,7 +169,7 @@ function draw_ghost(ctx, radius, options) {
     ctx.fillStyle = options.fill || "red";
     ctx.lineWidth || radius * 0.5;
     ctx.beginPath();
-    for(foot = 0; foot < feet; foot++) {
+    for (foot = 0; foot < feet; foot++) {
         ctx.arc(
             (2 * foot_radius * (feet - foot)) - head_radius - foot_radius,
             radius - foot_radius,
